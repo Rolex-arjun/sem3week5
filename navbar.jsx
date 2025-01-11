0bc4be78ed00458f095c8a5597e4ef96c77c3c94
Rolex-arjun <143622488+Rolex-arjun@users.noreply.github.com>
2025-01-11 12:32:38 +0530

    Create navbar.jsx

---
+++ navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => (
+  <nav className="navbar">
+    <h1>Spotify Clone</h1>
+    <ul>
+      <li>Home</li>
+      <li>Search</li>
+      <li>Playlists</li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
