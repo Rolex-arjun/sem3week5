3f56c195185384179aef9926ce8a36902ea43532
Rolex-arjun <143622488+Rolex-arjun@users.noreply.github.com>
2025-01-11 12:37:37 +0530

    Create Player.jsx

---
+++ Player.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Player.css';
+
+const Player = () => (
+  <div className="player">
+    <button>⏮️</button>
+    <button>⏯️</button>
+    <button>⏭️</button>
+  </div>
+);
+
+export default Player;
