b40e1ed7c661bfdf07bb06937e8e5cf1dde62dc3
Rolex-arjun <143622488+Rolex-arjun@users.noreply.github.com>
2025-01-11 12:34:13 +0530

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+  <div className="home">
+    <h2>Welcome to Spotify Clone</h2>
+    <p>Explore trending music and your playlists here.</p>
+  </div>
+);
+
+export default Home;
